d9a64d21a8185cd300bb8b7917c6dcefdc6c3ad2
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-07-07 17:07:49 +0100

    testing keyboard events tab and input values

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -44,4 +44,40 @@ describe("Counter", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
   })
+
+  it('renders a count of 10 after clicking the set button', async () => {
+    user.setup()
+    render(<Counter />)
+
+    const amountInput = screen.getByRole('spinbutton')
+    // from Utility API https://testing-library.com/docs/user-event/utility/
+
+    await user.type(amountInput, '10')
+    expect(amountInput).toHaveValue(10)
+    const  setButton = screen.getByRole('button', {
+        name: 'Set',
+    })
+    await user.click(setButton)
+    const countElement = screen.getByRole('heading')
+    expect(countElement).toHaveTextContent('10')
+  })
+
+  it('focuses in the correct order for keyboard users',async () => {
+    user.setup()
+    render(<Counter />)
+
+    const amountInput = screen.getByRole('spinbutton')
+    const setButton = screen.getByRole('button', { name: 'Set' })
+    const incrementButton = screen.getByRole('button', { name: 'Increment'})
+
+
+    // From convenience API https://testing-library.com/docs/user-event/convenience/
+    await user.tab()
+    expect(incrementButton).toHaveFocus()
+    await user.tab()
+    expect(amountInput).toHaveFocus()
+    await user.tab()
+    expect(setButton).toHaveFocus()
+    // Not used, but copy, paste and cut are in the Clipboard APIs https://testing-library.com/docs/user-event/clipboard/
+  })
 });
